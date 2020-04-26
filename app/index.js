ebe3a44ec68d86785dc66f85525c5111b0143bd4
swaggerloaf <swaggerloaf@gmail.com>
2020-04-26 13:11:35 -0400

    updates

--- app/index.js
+++ app/index.js
@@ -1,19 +1,15 @@
 import * as React from 'react';
-import ReactDom from 'react-dom';
+import * as ReactDom from 'react-dom';
 import './stylesheets/main.scss';
 
-// Dom element and react element
-const Button = ({ text }) => (
-  <button
-    onClick={() => alert('You had me at change!!!!!')}
-    type="button"
-    className="btn btn-dark"
-  >
-    <span>{text}</span>
-  </button>
-);
+function Pokemon() {
+  let pokémon = 'Brian';
+  return (
+    <div className="pokemon">
+      Hello, <span>{pokémon}</span>!
+    </div>
+  );
+}
 
-ReactDom.render(
-  <Button text={'Change the World Again'} />,
-  document.getElementById('mountNode')
-);
+const domElement = document.getElementById('root');
+ReactDom.render(<Pokemon />, domElement);
